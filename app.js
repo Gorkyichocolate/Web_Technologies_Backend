660ab3e22d2eb851db36a627c321cb070765f2d8
Gorkyi Chocolate <beybarskrutoi572@gmail.com>
2025-12-15 12:44:16 +0500

    1231312

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send(`
+  res.send(`
     <form method="POST" action="/calculate-bmi">
       <input name="weight" type="number" step="0.1" placeholder="Weight" required />
       <input name="height" type="number" step="0.01" placeholder="Height" required />
@@ -13,18 +13,19 @@ app.get('/', (req, res) => {
   `);
 });
 
+
 app.post('/calculate-bmi', (req, res) => {
-    const { weight, height } = req.body;
+  const { weight, height } = req.body;
 
-    const bmi = weight / ((height / 100) * (height / 100));
-    let category = '';
+  const bmi = weight / ((height / 100) * (height / 100));
+  let category = '';
 
-    if (bmi < 18.5) category = 'Underweight';
-    else if (bmi < 24.9) category = 'Normal';
-    else if (bmi < 29.9) category = 'Overweight';
-    else category = 'Obese';
+  if (bmi < 18.5) category = 'Underweight';
+  else if (bmi < 24.9) category = 'Normal';
+  else if (bmi < 29.9) category = 'Overweight';
+  else category = 'Obese';
 
-    res.send(`BMI: ${bmi.toFixed(2)} (${category})`);
+  res.send(`BMI: ${bmi.toFixed(2)} (${category})`);
 });
 
 app.listen(3000, () => console.log('OK'));
